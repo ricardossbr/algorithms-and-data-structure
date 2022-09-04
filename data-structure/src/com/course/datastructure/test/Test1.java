93f341037418a0fc55b07a2247e19de4ccfbb509
Ricardo Soares <riicardo.soares@gmail.com>
2022-09-04 20:05:18 -0300

    Work vector: add element.

---
+++ data-structure/src/com/course/datastructure/test/Test1.java
@@ -0,0 +1,14 @@
+package com.course.datastructure.test;
+
+import com.course.datastructure.vector.Vector;
+
+public class Test1 {
+
+    public static void main(String args []) {
+        Vector vector1 = new Vector(2);
+        vector1.add("Element 1");
+        vector1.add("Element 2");
+        vector1.add("Element 3");
+        System.out.println(vector1);
+    }
+}
---
+++ data-structure/src/com/course/datastructure/vector/Vector.java
@@ -0,0 +1,52 @@
+package com.course.datastructure.vector;
+
+public class Vector {
+
+    private String [] element;
+    private int size;
+
+    public Vector(int capacity){
+        this.element = new String[capacity];
+        this.size = 0;
+    }
+
+    // wrong way to implement this code... because it´s expend a lot off memory for add
+    /*public void add(String element){
+        for(int i=0; i<this.element.length; i++){
+            if(this.element[i] == null){
+                this.element[i] = element;
+                break;
+            }
+        }
+    }*/
+
+  /*  public void add(String element) throws Exception {
+        if(this.size < this.element.length){
+            this.element[this.size] = element;
+            this.size++;
+        }else {
+            throw new Exception("This Vector is full, is not possible to add more element!");
+        }
+    }*/
+
+    public boolean add(String element){
+        if(this.size < this.element.length){
+            this.element[this.size] = element;
+            this.size++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder print = new StringBuilder();
+        print.append("[");
+        for(int i=0; i<this.element.length; i++){
+            print.append(this.element[i]);
+            if(i<(this.element.length -1)) print.append(",");
+        }
+        print.append("]");
+        return print.toString();
+    }
+}
